fbd14043e56864d1e9a0642691ea90ead3a11ae9
Harsh Mishra <154444345+erharshmishra@users.noreply.github.com>
2025-12-24 12:36:18 +0530

    Add files via upload

---
+++ 296.Export_in_File/math.js
@@ -0,0 +1,15 @@
+const sum = (a, b) => a + b;
+const subtract = (a, b) => a - b;
+const multiply = (a, b) => a * b;
+const divide = (a, b) => a / b;
+const square = (a) => a * a;
+const squareRoot = (a) => Math.sqrt(a);
+
+module.exports = {
+  sum,
+  subtract,
+  multiply,
+  divide,
+  square,
+  squareRoot,
+};
---
+++ 296.Export_in_File/script.js
@@ -0,0 +1,2 @@
+const math = require("./math.js");
+console.log(math.sum(5, 3));
