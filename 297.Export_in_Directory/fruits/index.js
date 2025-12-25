d68ef05944fbc793abe20974b0cc8ae078f0410a
Harsh Mishra <154444345+erharshmishra@users.noreply.github.com>
2025-12-25 13:05:13 +0530

    Add files via upload

---
+++ 297.Export_in_Directory/fruits/index.js
@@ -0,0 +1,8 @@
+const apple = require("./apple.js");
+const banana = require("./banana.js");
+const mango = require("./mango.js");
+const orange = require("./orange.js");
+
+let fruits = [apple, banana, mango, orange];
+
+module.exports = fruits;
