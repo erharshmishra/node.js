297d08de2203a8859362c721e054c7f8df864edd
Harsh Mishra <154444345+erharshmishra@users.noreply.github.com>
2025-12-27 11:15:06 +0530

    Add files via upload

---
+++ 298.Import/math.js
@@ -0,0 +1,6 @@
+export const sum = (a, b) => a + b;
+export const subtract = (a, b) => a - b;
+export const multiply = (a, b) => a * b;
+export const divide = (a, b) => a / b;
+export const square = (a) => a * a;
+export const squareRoot = (a) => Math.sqrt(a);
---
+++ 298.Import/script.js
@@ -0,0 +1,6 @@
+// import { sum } from './math.js';
+// import { subtract } from './math.js';
+import { sum, subtract } from "./math.js";
+
+console.log(sum(2, 3));
+console.log(subtract(2, 3));
